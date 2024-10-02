0c0ccf78dc1b9f97751fb990ba7f7101a8f884a9
soohyun <developer.shkim@gmail.com>
2024-10-02 20:57:48 +0900

    refactor: 불필요한 파일 삭제

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CompetencyModule } from './modules/competency/competency.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), CompetencyModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
