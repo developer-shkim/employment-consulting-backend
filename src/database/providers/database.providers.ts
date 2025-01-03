6f6b6e6fbe847cd352307edf11cb5cc2af76c614
soohyun <developer.shkim@gmail.com>
2025-01-03 23:07:30 +0900

    chore: 환경변수 분리

--- src/database/providers/database.providers.ts
+++ src/database/providers/database.providers.ts
@@ -16,11 +16,11 @@ export const databaseProviders = [
     useFactory: async () => {
       const dataSource = new DataSource({
         type: 'mysql',
-        host: 'localhost',
-        port: 3306,
-        username: 'admin',
-        password: 'password',
-        database: 'emp_consulting',
+        host: process.env.DATABASE_HOST,
+        port: process.env.DATABASE_PORT as unknown as number,
+        username: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
         entities: [
           UserOrmEntity,
           CareerOrmEntity,
